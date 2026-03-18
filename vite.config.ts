00fe2fcd88498579c2fc40d20ea4d4bbd7165efb
Hafsa <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-18 18:17:33 +0000

    feat: portfolio v1

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // <--- Ajoute cet alias
+      "@": path.resolve(__dirname, "./src"), // <--- Ajout decet alias
     },
   },
 });
