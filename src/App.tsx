8624d94258e4cdca02fe4dc20fa6734c849a700e
Hafsa <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-18 18:48:40 +0000

    feat: analytics activés

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Analytics } from "@vercel/analytics/react"; 
+
 function App() {
   return <h1 className="bg-red-500 text-white p-10">Test Tailwind</h1>;
+     <Analytics />
+
 }
 
 export default App;
